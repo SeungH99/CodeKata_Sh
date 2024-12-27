6d0f0ba4dc852001c0ebb743f83f7a3d003c104d
지승현 <101084573+201810813@users.noreply.github.com>
2024-12-27 14:25:43 +0900

    Update 자릿수 더하기.cpp

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cpp
@@ -4,14 +4,10 @@ using namespace std;
 int solution(int n)
 {
     int result = 0;
-    result += n / 100000000;
-    result += (n % 100000000) / 10000000;
-    result += (n % 10000000) / 1000000;
-    result += (n % 1000000) / 100000;
-    result += (n % 100000) / 10000;
-    result += (n % 10000) / 1000;
-    result += (n % 1000) / 100;
-    result += (n % 100) / 10;
-    result += (n % 10) / 1;
+
+    while(n > 0){
+        result += (n % 10);
+        n = n / 10;
+    }
     return result;
 }
