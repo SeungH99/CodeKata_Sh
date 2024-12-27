329b3053efe4fa0f18c392470038e4ab852ef8e5
지승현 <101084573+201810813@users.noreply.github.com>
2024-12-27 15:03:32 +0900

    Update 나머지가 1이 되는 수 찾기.cpp

--- 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -5,13 +5,11 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    vector<int> vec;
     for(int i = 1; i <= n; ++i){
         if(n % i == 1){
-            vec.push_back(i);
+            answer = i;
+            break;
         }
     }
-    sort(vec.begin(), vec.end());
-    answer = *vec.begin();
     return answer;
 }
