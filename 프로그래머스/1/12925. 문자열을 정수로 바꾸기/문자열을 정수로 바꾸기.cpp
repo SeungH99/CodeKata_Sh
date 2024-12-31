8753b83a93830ef8e41468532c0ffe17f2ab5a37
지승현 <101084573+201810813@users.noreply.github.com>
2024-12-31 10:39:01 +0900

    Update 문자열을 정수로 바꾸기.cpp

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.cpp
@@ -4,5 +4,20 @@
 using namespace std;
 
 int solution(string s) {
-    return stoi(s);
+    int sign = 1;
+    int num = 0;
+    int start = 0;
+    if(s[0] == '+'){
+        sign = 1;
+        start = 1;
+    }
+    else if(s[0] == '-'){
+        sign = -1;
+        start = 1;
+    }
+    for(int i = start; i < s.size(); ++i){
+        num = num * 10 + (s[i] - '0');
+    }
+    
+    return sign * num;
 }
