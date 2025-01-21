c6be98f6a28d1967ca2826980dca138ecc844aff
지승현 <101084573+201810813@users.noreply.github.com>
2025-01-21 09:25:42 +0900

    [level 1] Title: 시저 암호, Time: 0.15 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s, int n) {
+    string answer = "";
+
+    for (char c : s) {
+        if (islower(c)) {
+            answer += (c - 'a' + n) % 26 + 'a';
+        } 
+        else if (isupper(c)){
+            answer += (c - 'A' + n) % 26 + 'A';
+        } 
+        else {
+            answer += c;
+        }
+    }
+    return answer;
+}
